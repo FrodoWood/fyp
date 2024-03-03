61f35aa35ad7daa08f1923179b326a03a3339561
Jasraj Singh <elijahbaggins3@gmail.com>
2024-03-03 20:19:10 +0000

    Added discrete actions.
    performance is on par with continuous for the most part, but training is slower due to lower batch size and buffer size.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -22,6 +22,8 @@ public class EnemyController : Agent
     public Transform bulletSpawner;
     private float actionCooldown = 0f;
     public float actionCooldownDuration = 0.5f;
+    private Vector3 xInput;
+    private Vector3 zInput;
     private Vector3 controlSignal = Vector3.zero;
 
 
@@ -47,6 +49,8 @@ public class EnemyController : Agent
         health = 50f;
         bulletSpawner.localPosition = getRandomPosition();
         actionCooldown = 0f;
+        xInput = Vector3.zero;
+        zInput = Vector3.zero;
         controlSignal = Vector3.zero;
 
         var myBullets = transform.parent.GetComponentsInChildren<Bullet>();
@@ -60,8 +64,8 @@ public class EnemyController : Agent
     {
         // agent position
         //sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(transform.localPosition.x/15f);
-        sensor.AddObservation(transform.localPosition.z/15f);
+        sensor.AddObservation(transform.localPosition.x / 15f);
+        sensor.AddObservation(transform.localPosition.z / 15f);
         //Debug.Log(transform.localPosition.x / 15f);
 
         // agent velocity
@@ -104,8 +108,13 @@ public class EnemyController : Agent
         
         if(actionCooldown <= 0f)
         {
-            controlSignal.x = actions.ContinuousActions[0];
-            controlSignal.z = actions.ContinuousActions[1];
+            //controlSignal.x = actions.ContinuousActions[0];
+            //controlSignal.z = actions.ContinuousActions[1];
+            int discreteAction0 = actions.DiscreteActions[0];
+            int discreteAction1 = actions.DiscreteActions[1];
+
+            discreteActionToControlSignal(discreteAction0, discreteAction1);
+            controlSignal = xInput + zInput;
 
             // Reset cooldown
             actionCooldown = actionCooldownDuration;
@@ -115,11 +124,43 @@ public class EnemyController : Agent
             actionCooldown -= Time.deltaTime;
         }
 
-        controller.Move(controlSignal * Time.deltaTime * moveSpeed);
+        controller.Move(controlSignal.normalized * Time.deltaTime * moveSpeed);
         // Rewards
         AddReward(1f / MaxStep);
-        if (StepCount == MaxStep) Debug.Log("Survived!");
+        if (StepCount == MaxStep)
+        {
+            //AddReward(0.5f);
+            Debug.Log("Survived!");
+        }
         
+    }
+
+    private void discreteActionToControlSignal(int discreteAction0, int discreteAction1)
+    {
+        switch (discreteAction0)
+        {
+            case 0:
+                xInput = Vector3.left;
+                break;
+            case 1:
+                xInput = Vector3.right;
+                break;
+            case 2:
+                xInput = Vector3.zero;
+                break;
+        }
+        switch (discreteAction1)
+        {
+            case 0:
+                zInput = Vector3.back;
+                break;
+            case 1:
+                zInput = Vector3.forward;
+                break;
+            case 2:
+                zInput = Vector3.zero;
+                break;
+        }
     }
 
     private void Update()
@@ -144,6 +185,7 @@ public class EnemyController : Agent
             //AddReward(-0.5f);
             //AddReward(-0.5f);
             //float currentReward = GetCumulativeReward();
+            //SetReward(-1f);
             //if (currentReward < 0f)
             //{
             //    SetReward(0f);
@@ -156,7 +198,7 @@ public class EnemyController : Agent
         {
             //AddReward(-0.1f);
             //AddReward(-(currentReward / 3));
-            //AddReward(-1);
+            //SetReward(-1);
             //float currentReward = GetCumulativeReward();
             //if (currentReward < 0f)
             //{
